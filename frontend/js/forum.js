34e543a6923f2190f0907a200a17e43053cbe4d8
Badzso <norbert.ujj@gmail.com>
2021-06-22 17:54:41 +0200

    forum 0.2

--- frontend/js/forum.js
+++ frontend/js/forum.js
@@ -1,39 +1,52 @@
+let kI = 7;
+let tI = 0;
+let cI = 0;
 var app = new Vue({
     el: '#main',
     data: {
+        //felhasználó adatok
+        felhasznalo: '',
+        beosztas: '',
+
+        //oldallal kapcsolatos
         kategoria: [
-            {id: 0,cim: 'Autók'},
-            {id: 1,cim: 'Motorok'},
-            {id: 2,cim: 'Szerelés'},
-            {id: 3,cim: 'Cruising'},
-            {id: 4,cim: 'Találkozók'},
-            {id: 5,cim: 'Társalgás'},
-            {id: 6,cim: 'Egyéb'},
+            {id: 0,cim: 'Autók', date: '2021.03.01'},
+            {id: 1,cim: 'Motorok', date: '2021.03.01'},
+            {id: 2,cim: 'Szerelés', date: '2021.03.01'},
+            {id: 3,cim: 'Cruising', date: '2021.03.01'},
+            {id: 4,cim: 'Találkozók', date: '2021.03.01'},
+            {id: 5,cim: 'Társalgás', date: '2021.03.01'},
+            {id: 6,cim: 'Egyéb', date: '2021.03.01'},
         ],
         topikok: [
-            {kId: 0,id: 0,cim: 'Kezdő autók'},
-            {kId: 0,id: 1,cim: 'Verseny autók'},
-            {kId: 0,id: 2,cim: 'Drift autók'},
-            {kId: 0,id: 3,cim: 'Terep autók'},
-            {kId: 1,id: 4,cim: 'Kezdő motorok'},
-            {kId: 1,id: 5,cim: 'Sport motorok'},
-            {kId: 1,id: 6,cim: 'Endúró motorok'},
-            {kId: 1,id: 7,cim: 'Gyorsasági motorok'},
-            {kId: 2,id: 8,cim: 'Suzuki swift motorcsere'},
-            {kId: 2,id: 9,cim: 'Suzuki Jimny motorcsere'},
-            {kId: 2,id: 10,cim: 'BMW difi csere'},
+            {kId: 0,id: 0,cim: 'Kezdő autók', date: '2021.03.01'},
+            {kId: 0,id: 1,cim: 'Verseny autók', date: '2021.03.01'},
+            {kId: 0,id: 2,cim: 'Drift autók', date: '2021.03.01'},
+            {kId: 0,id: 3,cim: 'Terep autók', date: '2021.03.01'},
+            {kId: 1,id: 4,cim: 'Kezdő motorok', date: '2021.03.01'},
+            {kId: 1,id: 5,cim: 'Sport motorok', date: '2021.03.01'},
+            {kId: 1,id: 6,cim: 'Endúró motorok', date: '2021.03.01'},
+            {kId: 1,id: 7,cim: 'Gyorsasági motorok', date: '2021.03.01'},
+            {kId: 2,id: 8,cim: 'Suzuki swift motorcsere', date: '2021.03.01'},
+            {kId: 2,id: 9,cim: 'Suzuki Jimny motorcsere', date: '2021.03.01'},
+            {kId: 2,id: 10,cim: 'BMW difi csere', date: '2021.03.01'},
         ],
         kommentek: [
-            {uId: 0,tId: 0,id: 1,cim: 'Kurva jó kocsi'},
-            {uId: 1,tId: 0,id: 2,cim: 'Nekem nem tetszik'},
-            {uId: 0,tId: 0,id: 3,cim: 'Eskü gyári'},
-            {uId: 1,tId: 0,id: 4,cim: 'Hiszi a piszi'},
-            {uId: 2,tId: 1,id: 5,cim: 'Jók a felnik'},
-            {uId: 3,tId: 1,id: 6,cim: 'köszi kurva drága volt'},
-            {uId: 2,tId: 1,id: 7,cim: 'megérte'},
+            {uId: 0,tId: 0,id: 1,cim: 'Kurva jó kocsi', date: '2021.03.01'},
+            {uId: 1,tId: 0,id: 2,cim: 'Nekem nem tetszik', date: '2021.03.01'},
+            {uId: 0,tId: 0,id: 3,cim: 'Eskü gyári', date: '2021.03.01'},
+            {uId: 1,tId: 0,id: 4,cim: 'Hiszi a piszi', date: '2021.03.01'},
+            {uId: 2,tId: 1,id: 5,cim: 'Jók a felnik', date: '2021.03.01'},
+            {uId: 3,tId: 1,id: 6,cim: 'köszi kurva drága volt', date: '2021.03.01'},
+            {uId: 2,tId: 1,id: 7,cim: 'megérte', date: '2021.03.01'},
         ],
         Btopikok: false,
         Bkommentek: false,
+        moderalas: false,
+        Szmoderalas: 'Moderálás',
+        katAdd: false,
+        katSzoveg: 'Kategória hozzáadása',
+        ujKat: ''
     },
     mounted: function() {
         console.log("Lekérem a kategóriákat a szerverről");
@@ -45,8 +58,21 @@ var app = new Vue({
             .then(data => console.log(data))
             .catch(err => console.error(err));
         */
+       this.felhasznalolekeres();
     },
     methods: {
+        felhasznalolekeres: function(x) {
+            
+            /*
+            fetch('http://example.com/songs')
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err));
+            */
+
+            this.felhasznalo = 'Szamjli01';
+            this.beosztas = 'admin';        
+        },
         katValasztas: function(x) {
             console.log("Cím:" + this.kategoria[x].cim + ", ID: " + this.kategoria[x].id);
             this.Btopikok = true;
@@ -63,9 +89,9 @@ var app = new Vue({
             console.log("Cím:" + this.topikok[x].cim + ", ID: " + this.topikok[x].id);
             this.Bkommentek = true;
             this.kommentek.push(
-                {uId: 4,tId: 2,id: 5,cim: 'Jók a tükrök'},
-                {uId: 5,tId: 2,id: 6,cim: 'köszi, de gyári'},
-                {uId: 4,tId: 2,id: 7,cim: 'de attól még jó'}
+                {uId: 4,tId: 2,id: 5,cim: 'Jók a tükrök', date: '2021.03.01'},
+                {uId: 5,tId: 2,id: 6,cim: 'köszi, de gyári', date: '2021.03.01'},
+                {uId: 4,tId: 2,id: 7,cim: 'de attól még jó', date: '2021.03.01'}
             )
             //Kommentek lekérése a szerverről, majd push a kommentek Objectbe
             /*
@@ -74,6 +100,36 @@ var app = new Vue({
             .then(data => console.log(data))
             .catch(err => console.error(err));
             */
+        },
+        oldalModeralas: function() {
+            console.log('Az oldal átváltott moderátori üzemmódba!')
+            this.moderalas = !this.moderalas;
+            if(this.moderalas)
+            {
+                this.Szmoderalas = 'Mentés'
+            }
+            else
+            {
+                this.Szmoderalas = 'Moderálás'
+            }
+        },
+        katHozzaadas: function() {
+            this.katAdd = !this.katAdd;
+            if(this.katAdd)
+            {
+                this.katSzoveg = 'Mentés'
+            }
+            else
+            {
+                this.katSzoveg = 'Kategória hozzáadása'
+            }
+            
+            if(!this.katAdd)
+            {
+                this.kategoria.push({id: kI,cim: this.ujKat, date: '2021.03.01'});
+                this.ujKat = '';
+                kI++;
+            }
         }
     }
 })
